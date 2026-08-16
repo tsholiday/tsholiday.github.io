f69f6d3fa2319b6fcef49c5e1c49e8aae383dc21
Tsholiday <tsholidaybandung@gmail.com>
2026-08-16 09:59:36 +0700

    Remove about_desc from translations
    Removed 'about_desc' from English and Indonesian translations.

---
+++ v1/i18n.js
@@ -0,0 +1,8 @@
+const translations = {
+    "en": {
+        "about_title": "Your Exclusive Travel Partner",
+    },
+    "id": {
+        "about_title": "Mitra Perjalanan Eksklusif Anda",
+    }
+};
